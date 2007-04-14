443bae378d433605b7919859b9ae0ea1f582f3d5
paul.cantrell <paul.cantrell@ed9b18f1-2525-0410-8641-1396461ec383>
2007-04-14 21:00:55 +0000

    Oops! Checked in new files for testing exceptions.

---
+++ trunk/core-java/test/java/net/innig/sweetxml/ConverterOutputTest.java
@@ -0,0 +1,32 @@
+package net.innig.sweetxml;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+public class ConverterOutputTest
+    extends ConverterTest
+    {
+    private String expectedOutput;
+
+    public ConverterOutputTest(
+             String name,
+             String input,
+             String expectedOutput,
+             ConversionMode mode)
+        {
+        super(name, input, mode);
+        this.expectedOutput = trim(expectedOutput);
+        }
+    
+    @Test
+    public void testConversion()
+        throws IOException
+        {
+        String output = convert();
+        assert expectedOutput.equals(output)
+            : "Mismatched conversion result."
+                + "\n\nExpected:\n" + showInvisibles(expectedOutput)
+                + "\n\nActual:\n" + showInvisibles(output);
+        }
+    }
