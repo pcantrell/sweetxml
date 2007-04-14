aae84d9f287911b96a99781e0afd15fa62194f36
paul.cantrell <paul.cantrell@ed9b18f1-2525-0410-8641-1396461ec383>
2007-04-14 21:00:55 +0000

    Oops! Checked in new files for testing exceptions.

---
+++ core-java/test/java/net/innig/sweetxml/ParseExceptionTest.java
@@ -0,0 +1,47 @@
+package net.innig.sweetxml;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.fail;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.testng.annotations.Test;
+
+public class ParseExceptionTest
+    extends ConverterTest
+    {
+    private SweetXmlMessage expectedMessage;
+    private String expectedMessageArgs;
+    private DocumentPosition expectedPosition;
+
+    public ParseExceptionTest(
+            String name,
+            String input,
+            SweetXmlMessage expectedMessage,
+            String expectedMessageArgs,
+            DocumentPosition expectedPosition,
+            ConversionMode mode)
+        {
+        super(name, input, mode);
+        this.expectedMessage = expectedMessage;
+        this.expectedMessageArgs = expectedMessageArgs;
+        this.expectedPosition = expectedPosition;
+        }
+    
+    @Test
+    public void testParseExcecption()
+        throws IOException
+        {
+        try {
+            String output = convert();
+            fail("Expected parse exception, but got output:\n" + showInvisibles(output));
+            }
+        catch(SweetXmlParseException pe)
+            {
+            assertEquals(expectedMessage, pe.getMessageId());
+            assertEquals(expectedMessageArgs, Arrays.toString(pe.getMessageArgs()));
+            assertEquals(expectedPosition, pe.getPosition());
+            }
+        }
+    }
